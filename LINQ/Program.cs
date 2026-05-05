b8d22b93ca6bb521b84a53910b856e1c24af0c26
Monica Washington <monica.lynnwashington@gmail.com>
2026-05-05 10:19:47 -0600

    Completed Linq Exercise 2

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace LINQ
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var videoGames = new List<string>()
+            {
+                "Sonic the Hedgehog",
+                "The Legend of Zelda",
+                "Minecraft",
+                "Super Mario Bros",
+                "Fortnite",
+                "ProAm"
+            };
+            videoGames.OrderBy(x => x.Length).ToList().ForEach(x => Console.WriteLine(x));
+            
         }
     }
 }
